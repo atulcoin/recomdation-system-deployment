b50431103636712fc74bec956d4c2283075b66d8
“atulcoin” <atul10121@gmail.com>
2020-10-25 21:23:34 +0530

    started deployment of recomdation system

---
+++ app2/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+
+choice=[('atul','Atul'),('priya','priya'),('nisha','Nisha')]
+class inputdata(forms.Form):
+
+    first_name = forms.CharField( max_length = 200, widget=forms.Select(choices=choice))
+    addresss = forms.CharField( max_length = 200)
+    salary=forms.IntegerField(help_text='enter the salary')
+    status=forms.BooleanField(initial=False, required=False)
+
